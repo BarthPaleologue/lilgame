e3773921f54a4a2780e5e95271b91837646413d2
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2022-09-07 09:32:35 +0200

    removed useless js

--- src/ts/index.js
+++
@@ -1,7 +0,0 @@
-import { Canvas } from "./canvas";
-import { Game } from "./game";
-export function start(canvasElement) {
-    let canvas = new Canvas(canvasElement);
-    let game = new Game(canvas);
-    game.start();
-}
